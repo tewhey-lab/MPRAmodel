40a13f347da7e717d04efafa703bb49bf6d34f11
Hannah Dewey <57405854+deweyhannah@users.noreply.github.com>
2021-10-04 09:51:54 -0400

    Update and rename OL13_analysis_sub.R to Examples/OL13_analysis_sub.R

---
+++ Examples/OL13_analysis_sub.R
@@ -0,0 +1,19 @@
+
+#Read in Count and Attributes tables
+attr_PROJ <- read.delim("full/path/to/attributes/file", stringsAsFactors=F)
+count_PROJ <- read.delim("/full/path/to/count/table", stringsAsFactors=F)
+
+#Create Condition Table if not made in the MPRAcount pipeline
+#This is an example of what it would look like with 
+
+#total_reps <- 4
+#cond_PROJ <- as.data.frame(c(rep("DNA",total_reps), rep("K562",total_reps)), stringsAsFactors=F)
+#colnames(cond_PROJ) <- "condition"
+#rownames(cond_PROJ) <- colnames(count_PROJ)[7:ncol(count_PROJ)]
+
+source("/full/path/to/repository/tag_analysis_package.R")
+
+#Run the analysis
+tagWrapper(count_PROJ, attr_PROJ, cond_PROJ, filePrefix="example_prefix")
+
+writeLines(capture.output(sessionInfo()), "sessionInfo.txt")
